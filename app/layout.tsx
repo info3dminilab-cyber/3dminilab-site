ddf52141ff4761914a4ec69890c7d023cd764aeb
Lukasz Stendera <info.3dminilab@gmail.com>
2026-05-20 13:22:53 +0300

    Add SEO, robots and sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,36 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "3D MiniLAB",
-  description: "Инженерная 3D-печать для ваших задач",
+  description:
+    "Инженерная 3D-печать, прототипирование и производство деталей на заказ.",
+  keywords: [
+    "3D печать",
+    "3D printing",
+    "3D MiniLAB",
+    "Bambu Lab",
+    "прототипирование",
+    "3D печать Беларусь",
+    "3D печать Гомель",
+    "3D modeling",
+    "изготовление деталей",
+  ],
+
+  openGraph: {
+    title: "3D MiniLAB",
+    description:
+      "Инженерная 3D-печать, прототипирование и производство деталей на заказ.",
+    url: "https://3dminilab.by",
+    siteName: "3D MiniLAB",
+    locale: "ru_RU",
+    type: "website",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  metadataBase: new URL("https://3dminilab.by"),
 };
 
 export default function RootLayout({
@@ -27,7 +56,9 @@ export default function RootLayout({
       lang="ru"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-white">{children}</body>
+      <body className="min-h-full flex flex-col bg-white">
+        {children}
+      </body>
     </html>
   );
 }
